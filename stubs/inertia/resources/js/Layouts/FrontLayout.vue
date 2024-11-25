cae2dd2468bc5e24166dc7e25dcd52e5f3333086
helwy <hassanelwy@gmail.com>
2024-11-25 20:56:27 +0100

    Change FrontLayout background and fix issue with overextended width

--- stubs/inertia/resources/js/Layouts/FrontLayout.vue
+++ stubs/inertia/resources/js/Layouts/FrontLayout.vue
@@ -10,9 +10,7 @@ const logout = () => {
 
 <template>
     <Head title="Welcome"/>
-    <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-        <img id="background" class="absolute -left-20 top-0 max-w-[877px]"
-             src="https://laravel.com/assets/img/welcome/background.svg"/>
+    <div class="bg-gradient-to-tl from-green-300 to-white dark:from-green-950 dark:to-black text-black/50 dark:text-white/50">
         <div
             class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
